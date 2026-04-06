54a093f062b38465f9e5f43076858abd2a5f6d2b
otabek <otabekbarotov15@gmail.com>
2026-04-06 12:08:50 +0500

    templates folder moved to app

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Otabek'}
+    posts = [
+        {
+            'author': {'username': 'Abdulaziz'},
+            'body': 'Beautiful day in Tajikistan!'
+        },
+        {
+            'author': {'username': 'Ahmadjon'},
+            'body': 'The Invincible series was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
