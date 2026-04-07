f30e37b340fcf21f01afb04a548a27d9dd27a661
otabek <otabekbarotov15@gmail.com>
2026-04-07 12:05:22 +0500

    index page changed

--- app/routes.py
+++ app/routes.py
@@ -5,10 +5,10 @@ from app.forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
+    user = {'username': 'Otabek'}
     posts = [
-        {'author': {'username': 'John'}, 'body': 'Beautiful day in Portland!'},
-        {'author': {'username': 'Susan'}, 'body': 'The Avengers movie was so cool!'}
+        {'author': {'username': 'Abdulaziz'}, 'body': 'Beautiful day in Dushanbe!'},
+        {'author': {'username': 'Ahmadjon'}, 'body': 'The Invincible series was so cool!'}
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
