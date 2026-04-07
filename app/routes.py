515ee118e5b13e7a5cf4a61630dfb61a8111e18a
otabek <otabekbarotov15@gmail.com>
2026-04-07 11:49:30 +0500

    routes.py /index updated

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,16 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
-# ...
-
-@app.route('/login')
-def login():
-    form = LoginForm()
-    return render_template('login.html', title='Sign In', form=form)
-
-from flask import render_template, flash, redirect
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+        {'author': {'username': 'John'}, 'body': 'Beautiful day in Portland!'},
+        {'author': {'username': 'Susan'}, 'body': 'The Avengers movie was so cool!'}
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -17,5 +18,5 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
