5ff7af5fbf8785c4ff68f8a169869980bb894cbb
otabek <otabekbarotov15@gmail.com>
2026-04-27 11:57:37 +0500

    profile picture and profile edit functions added, 404 and 500 erros as well

--- app/models.py
+++ app/models.py
@@ -5,6 +5,8 @@ from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
+from hashlib import md5
+from datetime import datetime,timezone
 
 class User(UserMixin,db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -19,6 +21,14 @@ class User(UserMixin,db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
+    
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc))
     
     @login.user_loader
     def load_user(id):
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,12 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for,request
 from flask_login import current_user, login_user,logout_user,login_required
 import sqlalchemy as sa
 from app import db
-from app.forms import RegistrationForm
+from app.forms import RegistrationForm,EditProfileForm
 from app.models import User
 from app import app
 from app.forms import LoginForm
+from datetime import datetime,timezone
 
 @app.route('/')
 @app.route('/index')
@@ -17,6 +18,22 @@ def index():
         {'author': {'username': 'Ahmadjon'}, 'body': 'The Invincible series was so cool!'}
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.now(timezone.utc)
+        db.session.commit()
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -47,6 +64,23 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm(current_user.username)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
 
 @app.route('/logout')
 def logout():
